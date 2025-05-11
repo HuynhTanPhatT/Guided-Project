81c75c414051d5bc11937de979820c34e68c0871
HuynhTanPhatT <phat.tanhuynh2412@gmail.com>
2025-05-11 10:04:02 +0700

    Add files via upload

---
+++ project1/Beginner - Having & Where.sql
@@ -0,0 +1,14 @@
+﻿--Having & Where
+-- Having: Filters the aggregated data - từng nhóm - Dùng sau group by
+-- Where: Filters the base data - Từng dòng riêng lẻ - dùng trước group 
+select gender, avg(age)
+from dbo.employee_demographics
+--where avg(age) > 40 #wrong
+group by gender
+having avg(age)>40
+
+select occupation, AVG(salary) as AVG
+from dbo.employee_salary
+where occupation like '%City%'
+group by occupation
+having AVG(salary) > 50000
