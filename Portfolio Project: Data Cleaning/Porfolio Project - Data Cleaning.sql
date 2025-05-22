2b2adf052ae61b98f088b174fe4aef5c5290a8aa
Huynh Tan Phat <phat.tanhuynh2412@gmail.com>
2025-05-23 01:52:05 +0700

    Update Porfolio Project - Data Cleaning.sql

--- Portfolio Project: Data Cleaning/Porfolio Project - Data Cleaning.sql
+++ Portfolio Project: Data Cleaning/Porfolio Project - Data Cleaning.sql
@@ -28,18 +28,18 @@ where table_name = 'layoff_copy' and TABLE_SCHEMA = 'dbo';
 /* Create a copy table: The reason to create a copy table, because if we make mistakes => destroy the raw data */
 /* In the workplace, we dont work on the raw data*/
 select * 
-into layoff_copy
+into layoff_copy1
 from dbo.layoff;
 
 select *
-from dbo.layoff_copy;
+from dbo.layoff_copy1;
 
 /* 1.Identify Duplicates: 'Cazoo',...*/
 With duplicates_cte as (
 select	*,
 		row_number() over (partition by company, location, total_laid_off,date, 
 										percentage_laid_off, industry, stage, funds_raised, country  order by date) as row_num
-from layoff_copy
+from layoff_copy1
 )
 delete
 from duplicates_cte
@@ -48,54 +48,54 @@ where row_num > 1;
 /* 2. Standardize the Data */
 select	company,
 		trim(company)
-from dbo.layoff_copy 
+from dbo.layoff_copy 1
 
 --a/Remove white spaces
-Update dbo.layoff_copy
+Update dbo.layoff_copy1
 set company = trim(company);
 
 --b/Remove ,Non-U.S & ,Raleigh & ,Victoria & ,New York City   || Malmö => Malmo / Düsseldorf => Dusseldorf  
 select distinct(location)
-from dbo.layoff_copy
+from dbo.layoff_copy1
 order by 1;
 
-update dbo.layoff_copy
+update dbo.layoff_copy1
 set location = replace(replace(replace(replace(replace(location, ',Non-U.S.', ''), ',Raleigh', ''), ',Raleigh', ''),',Victoria',''),',New York City','')
 
-update dbo.layoff_copy
+update dbo.layoff_copy1
 set location = 'Malmo'
 where location like 'Malmö'
 
-update dbo.layoff_copy
+update dbo.layoff_copy1
 set location = 'Dusseldorf'
 where location like 'Düsseldorf'
 
 --c/ convert a string to datetime [date column]
-update dbo.layoff_copy
+update dbo.layoff_copy1
 set date = cast(date as date);
 ---Change the format to date
-ALTER TABLE dbo.layoff_copy
+ALTER TABLE dbo.layoff_copy1
 alter column date DATE;
 
 /* 3. Null values and Blank Values */
 --a/ Note: Appsmith => Industry Null => Không thể thêm dữ liệu để hết null được bởi vì chỉ có 1 công ty  => Set to 'Other'
-update dbo.layoff_copy
+update dbo.layoff_copy1
 set industry = 'Other'
 where industry is null;
 
 select *
-from dbo.layoff_copy
+from dbo.layoff_copy1
 where industry is null or industry = ''
 
 
 --b/ "Total_laid_off" and "Percentage_laid_off".
 select *
-from dbo.layoff_copy
-where (total_laid_off is null or  percentage_laid_off is null) ;
+from dbo.layoff_copy1
+where (total_laid_off is null and  percentage_laid_off is null) ;
 
 
  --c/Stage: Null Values -> Unknown
- update dbo.layoff_copy
+ update dbo.layoff_copy1
  set stage = 'Unknown'
  where stage is null
 
@@ -103,8 +103,8 @@ where (total_laid_off is null or  percentage_laid_off is null) ;
 /* 4. Remove Any Columns or Rows which are not necessary */
 --a/ I think that total_laid_off and percentage_laid_off both are null should delete => no usefull information ...
 DELETE 
-from dbo.layoff_copy
-where total_laid_off is null or percentage_laid_off is null
+from dbo.layoff_copy1
+where total_laid_off is null and percentage_laid_off is null
 -----------------------------------------------------------------------------------------
 ---Rename Column
-exec sp_rename 'dbo.layoff_copy.funds_raised', 'funds_raised_milions'
+exec sp_rename 'dbo.layoff_copy1.funds_raised', 'funds_raised_milions'
