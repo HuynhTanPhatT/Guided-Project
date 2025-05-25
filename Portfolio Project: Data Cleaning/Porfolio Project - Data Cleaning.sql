b370a09ee8dbd08e65d2ae20fe3c629bb703fe2d
Huynh Tan Phat <phat.tanhuynh2412@gmail.com>
2025-05-25 18:59:53 +0700

    Add files via upload

---
+++ Portfolio Project: Data Cleaning/Porfolio Project - Data Cleaning.sql
@@ -0,0 +1,103 @@
+﻿-------Porfolio Project: Data Cleaning
+/* Data Description
+total_laid_off : Number of employees laid off
+date : Date of layoff
+percentage_laid_off :Percentage of employees laid off */
+-----------------------------------------------------------------------------------------
+--Check Data Type
+select column_name, data_type
+from information_schema.COLUMNS
+where table_name = 'layoff' and table_schema = 'dbo';
+
+select column_name, data_type
+from INFORMATION_SCHEMA.COLUMNS
+where table_name = 'layoff_copy' and TABLE_SCHEMA = 'dbo';
+-----------------------------------------------------------------------------------------
+--- 1. Remove Duplicates
+--- 2. Standardize the Data
+--- 3. Null Values or Blank Values
+--- 4. Remove Any Columns or Rows which are not necessary
+-----------------------------------------------------------------------------------------
+/* Create a copy table: The reason to create a copy table, because if we make mistakes => destroy the raw data */
+/* In the workplace, we dont work on the raw data*/
+select * 
+into layoff_copy
+from dbo.layoff;
+
+select *
+from dbo.layoff_copy;
+
+/* 1.Identify Duplicates: 'Cazoo',...*/
+With duplicates_cte as (
+select	*,
+		row_number() over (partition by company, location, total_laid_off,date, 
+										percentage_laid_off, industry, stage, funds_raised, country  order by date) as row_num
+from layoff_copy
+)
+delete
+from duplicates_cte
+where row_num > 1;
+
+/* 2. Standardize the Data */
+select	company,
+		trim(company)
+from dbo.layoff_copy
+
+--a/Remove white spaces
+Update dbo.layoff_copy
+set company = trim(company);
+
+--b/Remove ,Non-U.S & ,Raleigh & ,Victoria & ,New York City   || Malmö => Malmo / Düsseldorf => Dusseldorf  
+select distinct(location)
+from dbo.layoff_copy
+order by 1;
+
+update dbo.layoff_copy
+set location = replace(replace(replace(replace(replace(location, ',Non-U.S.', ''), ',Raleigh', ''), ',Raleigh', ''),',Victoria',''),',New York City','')
+
+update dbo.layoff_copy
+set location = 'Malmo'
+where location like 'Malmö'
+
+update dbo.layoff_copy
+set location = 'Dusseldorf'
+where location like 'Düsseldorf'
+
+--c/ convert a string to datetime [date column]
+update dbo.layoff_copy
+set date = cast(date as date);
+---Change the format to date
+ALTER TABLE dbo.layoff_copy
+alter column date DATE;
+
+/* 3. Null values and Blank Values */
+--a/ Note: Appsmith => Industry Null => Không thể thêm dữ liệu để hết null được bởi vì chỉ có 1 công ty  => Set to 'Other'
+update dbo.layoff_copy
+set industry = 'Other'
+where industry is null;
+
+select *
+from dbo.layoff_copy
+where industry is null or industry = ''
+
+
+--b/ "Total_laid_off" and "Percentage_laid_off".
+select *
+from dbo.layoff_copy1
+where (total_laid_off is null and  percentage_laid_off is null) ;
+
+
+ --c/Stage: Null Values -> Unknown
+ update dbo.layoff_copy
+ set stage = 'Unknown'
+ where stage is null
+
+
+/* 4. Remove Any Columns or Rows which are not necessary */
+--a/ I think that total_laid_off and percentage_laid_off both are null should delete => no usefull information ...
+DELETE 
+from dbo.layoff_copy
+where total_laid_off is null and percentage_laid_off is null
+-----------------------------------------------------------------------------------------
+---Rename Column
+exec sp_rename 'dbo.layoff_copy.funds_raised', 'funds_raised_milions'
---
+++ Portfolio Project: Data Cleaning/Portfolio Project - Exploratory Data Analysis.sql
@@ -0,0 +1,73 @@
+﻿-----Exploratory Data Analysis
+select *
+from dbo.layoff_copy
+
+------------------------------------------------------------------------------------------------
+/* Add a "Total employee before laid off" column */
+-- Total = laid_off / percentage => Percentage = (total / laid_off)*100 || laid_off = total * percentage
+-- Take all the values from left except '%':  left(percentage_laid_off,len(percentage_laid_off)-1)
+Alter table dbo.layoff_copy
+add "total_employee_before_laid_off" float
+
+UPDATE dbo.layoff_copy
+SET total_employee_before_laid_off = CAST(
+    NULLIF(total_laid_off, 0) / 
+    NULLIF(CAST(LEFT(percentage_laid_off, LEN(percentage_laid_off) - 1) AS FLOAT) / 100.0, 0)
+    AS INT
+);
+
+
+select *
+from dbo.layoff_copy
+------------------------------------------------------------------------------------------------
+select	 year(date) as year, sum(total_laid_off) as layoffs
+from layoff_copy
+group by year(date)
+order by year(date) asc;
+
+--The number of layoff happend in every month
+select	country,
+		COUNT(total_laid_off) as 'The total number of lay off',
+		sum(total_laid_off) as 'employee_laid_off',
+		month(date) as month, year(date) as year
+from dbo.layoff_copy
+group by country, month(date), year(date)
+order by year(date), month(date) asc;
+
+select *
+from dbo.layoff_copy
+where percentage_laid_off = 100
+order by total_laid_off desc;
+
+
+
+--Rolling lay offs with Date 
+-- Extract year 
+with rolling_total as (
+select	cast(year(date) as varchar(4)) + '-'+ right('0' + cast(month(date) as varchar(2)),2) as YearMonth,
+		sum(total_laid_off) as laid_off
+from dbo.layoff_copy
+group by month(date), year(date)
+)
+select	YearMonth, laid_off,
+		sum(laid_off) over(order by YearMonth) as RollingTotal, laid_off
+from	rolling_total;
+
+
+
+--Which Year They laid off the most
+WITH company_year as (
+select	company,
+		year(date) as year,
+		sum(total_laid_off) as total_laid_off
+from dbo.layoff_copy
+group by company, year(date)
+), Company_Year_Rank as (
+select	*,
+		DENSE_RANK() over( partition by year order by total_laid_off desc) as rank_c_laid_off
+from company_year
+where total_laid_off is not null
+)
+select *
+from Company_Year_Rank
+where rank_c_laid_off <=5;
